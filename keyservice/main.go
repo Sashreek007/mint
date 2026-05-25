de769d14a9d31587613fd316a0740b7fdd65f337
Sashreek007 <Sashreek.addanki@gmail.com>
2026-05-25 01:10:01 -0600

    added replicaID to be visible for loadbalancing

--- keyservice/main.go
+++ keyservice/main.go
@@ -3,19 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
+	"os"
 )
 
 func main() {
+	//HOSTNAME is automatically set by docker in every container to the containers short id 
+	
+	replicaID := os.Getenv("HOSTNAME")
+	if replicaID == ""{
+		replicaID = "local"
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"status":"ok"}` + "\n"))
+		fmt.Fprintln(w, `{"status":"ok","replica":"%s"}`, replicaID)
 	})
 
 	addr := ":8080"
-	log.Printf("keyservice listening on %s", addr)
+	log.Printf("keyservice replica=%s listening on %s", replicaID, addr)
 	if err := http.ListenAndServe(addr, mux); err != nil {
 		log.Fatalf("server failed: %v", err)
 	}
